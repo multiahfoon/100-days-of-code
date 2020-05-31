36ae8e4b58dc1a4390cd0f3576905592c9bc1261
Multi Ah Foon <multiahfoon@gmail.com>
2020-05-31 12:26:35 +1200

    DOM elements stored in variables

--- js/project-3.js
+++ js/project-3.js
@@ -1,2 +1,6 @@
 // create a quote generator
 // have all quotes stored into an array or maybe an object or something
+const genQuoteBtn = document.getElementById('genQuoteBtn');
+const quoteDisplay = document.getElementById('quoteDisplay');
+const author = document.getElementById('authors');
+
