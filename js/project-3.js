04ae8899b222bec202af7f2b030b76248f273f5a
Multi Ah Foon <multiahfoon@gmail.com>
2020-05-31 12:33:31 +1200

    added event listener that shifts through list of quotes then displays in DOM

--- js/project-3.js
+++ js/project-3.js
@@ -4,3 +4,7 @@ const genQuoteBtn = document.getElementById('genQuoteBtn');
 const quoteDisplay = document.getElementById('quoteDisplay');
 const author = document.getElementById('authors');
 
+const quoteList = ["asdf", "asdflkj", "dfdaskljljk"];
+genQuoteBtn.addEventListener('click', ()=>{
+    quoteDisplay.innerText = quoteList[Math.floor(Math.random()*3)];
+});
