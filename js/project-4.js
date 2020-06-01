d774bf13781f8780c35a8577a510af741c018eb1
Multi Ah Foon <multiahfoon@gmail.com>
2020-06-01 16:21:48 +1200

    can now access dom via id

--- js/project-4.js
+++ js/project-4.js
@@ -1,3 +1,5 @@
-/*
-    Objective: Take input then display it in dom
-*/
+// Objective: Take input then display it in dom
+// access elements in dom
+const messageInput = document.getElementById('messageInput');
+const messageBtn = document.getElementById('messageBtn');
+const messageDisplay = document.getElementById('messageDisplay');
