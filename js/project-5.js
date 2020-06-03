9561e29b5b80b5efc70f6d7c553b952e0faa2916
Multi Ah Foon <multiahfoon@gmail.com>
2020-06-04 11:25:31 +1200

    code refactor

--- js/project-5.js
+++ js/project-5.js
@@ -3,19 +3,13 @@ const lowerBtn = document.getElementById('lowerBtn');
 const addBtn = document.getElementById('addBtn');
 
 let currentNum = 0;
-countDisplay.innerText = currentNum; 
 
 lowerBtn.addEventListener('click', () => {
-    console.log("click");
     currentNum --;
-    console.log(currentNum);
     countDisplay.innerText = currentNum; 
-    return currentNum;});
+});
 
 addBtn.addEventListener('click', () => {
-    console.log("click");
     currentNum ++;
-    console.log(currentNum);
     countDisplay.innerText = currentNum; 
-    return currentNum;
 });
