f95a92606d78e4912cd530e49ea458c7d3c17024
Multi Ah Foon <multiahfoon@gmail.com>
2020-06-04 11:17:27 +1200

    added event listeners to buttons

--- js/project-5.js
+++ js/project-5.js
@@ -1,3 +1,14 @@
 const countDisplay = document.getElementById('countDisplay');
-const lowerBtn = document.getElementsByTagName('lowerBtn');
-const addBtn = document.getElementsByTagName('addBtn');
+const lowerBtn = document.getElementById('lowerBtn');
+const addBtn = document.getElementById('addBtn');
+
+let currentNum = 0;
+countDisplay.innerText = currentNum; 
+
+lowerBtn.addEventListener('click', () => {
+    console.log("click");
+});
+
+addBtn.addEventListener('click', () => {
+    console.log("click");
+});
