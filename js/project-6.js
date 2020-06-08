f9d7ed79ae73f60bb01a76f0c0f5c56d8977671b
Multi Ah Foon <multiahfoon@gmail.com>
2020-06-08 20:51:14 +1200

    fix bug where twoSums were the same value

--- js/project-6.js
+++ js/project-6.js
@@ -2,13 +2,13 @@
 const equationDisplay = document.getElementById('equationDisplay');
 
 // generate random numbers between 1 to 10
-let randomNum = Math.floor(Math.random()*11);
 let answer = false;
 let twoSums = [];
 
 // generate two values for equation
-for(let i = 0; i <= 1; i++){
- twoSums.push(randomNum);
+for(let i = 0; i < 2; i++){
+    let randomNum = Math.floor(Math.random()*11);
+    twoSums.push(randomNum);
 }
 console.log(twoSums);
 // display equations
