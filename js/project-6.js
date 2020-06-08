bcc1e4db71f7bfc35515e75d93f4333170a7b19c
Multi Ah Foon <multiahfoon@gmail.com>
2020-06-08 20:20:22 +1200

    two values can now be generated for equation

--- js/project-6.js
+++ js/project-6.js
@@ -1,5 +1,14 @@
 // Problem soliving app
+const equationDisplay = document.getElementById('equationDisplay');
 
-// generate random numbers between 1 - 10
+// generate random numbers between 1 to 10
+let randomNum = Math.floor(Math.random()*11);
+let answer = false;
+let twoSums = [];
 
-// generate equation
+// generate two values for equation
+for(let i = 0; i <= 1; i++){
+ twoSums.push(randomNum);
+}
+
+// display equations
