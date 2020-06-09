1030db8c9ebe4ae1feffd0286a2f048530d76637
Multi Ah Foon <multiahfoon@gmail.com>
2020-06-09 14:27:23 +1200

    can now check user guess if correct

--- js/project-6.js
+++ js/project-6.js
@@ -1,25 +1,38 @@
 // Problem soliving app
 const equationDisplay = document.getElementById('equationDisplay');
+const userGuess = document.getElementById('userGuess');
+const check = document.getElementById('check');
 
-// generate random numbers between 1 to 10
 let answer = false;
 let twoSums = [];
 
-// generate two values for equation
+// loop to generate two values for equation
 for(let i = 0; i < 2; i++){
+    // generate random numbers between 0 to 10 both non inclusive 
     let randomNum = Math.floor(Math.random()*9)+1;
     twoSums.push(randomNum);
+    console.log(randomNum);
+    console.log(twoSums);
 }
 
 // calculate total
 let total = eval(twoSums[0]+twoSums[1]);
 
-
 // display equation
-let equation = `${twoSums[0]} + ${twoSums[1]} = ?`;
+let equation = `${twoSums[0]} + ${twoSums[1]} =`;
 equationDisplay.innerText = equation;
 
-
-
-
-// display equations
+// auto select input field so user can type
+userGuess.focus();
+console.log(total);
+// check users guess
+check.addEventListener('click', () => {
+    let guess = userGuess.value;
+    console.log(guess);
+    if(guess == total){
+        console.log("you got it right");
+    }else{
+        // not working
+        userGuess.innerText = "";
+    }
+});
