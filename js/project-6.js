560f2fa8125883e0c524db3f3423c94f1c5390dd
Multi Ah Foon <multiahfoon@gmail.com>
2020-06-11 19:50:54 +1200

    game can now generate new equation when guess is correct

--- js/project-6.js
+++ js/project-6.js
@@ -6,21 +6,26 @@ const checkBtn = document.getElementById('checkBtn');
 let answer = false;
 let twoSums = [];
 
-// loop to generate two values for equation
-for(let i = 0; i < 2; i++){
-    // generate random numbers between 0 to 10 both non inclusive 
-    let randomNum = Math.floor(Math.random()*9)+1;
-    twoSums.push(randomNum);
-    console.log(randomNum);
-    console.log(twoSums);
+const genEquation = () => {
+
+    // loop to generate two values for equation
+    for(let i = 0; i < 2; i++){
+        // generate random numbers between 0 to 10 both non inclusive 
+        let randomNum = Math.floor(Math.random()*9)+1;
+        twoSums.push(randomNum);
+        console.log(randomNum);
+        console.log(twoSums);
+    }
+
+    // display equation
+    let equation = `${twoSums[0]} + ${twoSums[1]} =`;
+    equationDisplay.innerText = equation;
 }
+// used to start game
+genEquation();
 
-// calculate total
+// calculate total but need refactoring
 let total = eval(twoSums[0]+twoSums[1]);
-
-// display equation
-let equation = `${twoSums[0]} + ${twoSums[1]} =`;
-equationDisplay.innerText = equation;
 
 // auto select input field so user can type
 userGuess.focus();
@@ -31,19 +36,26 @@ const checkGuess = () => {
     let guess = userGuess.value;
     console.log(guess);
     if(guess == total){
+        console.log(total);
+        userGuess.value = "";
+        twoSums = [];
+        genEquation();
+        total = eval(twoSums[0]+twoSums[1]);
         console.log("you got it right");
+        console.log(total);
     }else{
         // not working
         userGuess.value = "";
         userGuess.focus();
     }
 }
+
 checkBtn.addEventListener('click', () => {
     checkGuess();
 });
 
-userGuess.addEventListener("keydown", (e) => {
-    if(e.keyCode === 13){
+userGuess.addEventListener("keydown", (key) => {
+    if(key.keyCode === 13){
         checkGuess();
         console.log("enter");
     }
